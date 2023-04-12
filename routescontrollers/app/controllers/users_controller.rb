53681252ebb69a64fe6f5673e5ddb6b70bb6dc6a
root <somra45@gmail.com>
2023-04-12 02:23:23 -0400

    finish update

--- routescontrollers/app/controllers/users_controller.rb
+++ routescontrollers/app/controllers/users_controller.rb
@@ -16,13 +16,56 @@ class UsersController < ApplicationController
     end
 
     def show
-        render json: params
+ 
         if params[:name]
             user = User.find_by(name: params[:name])
+            if user
+                render json: user
+            else
+                render 'User not in database'
+            end
         elsif params[:email]
             user = User.find_by(email: params[:email])
+            if user
+                render json: user
+            else
+                render 'User not in database'
+            end
+        else
+            user = User.find_by(id: params.values.first)
+            if user
+                render json: user
+            else
+                render 'User not in database'
+            end
         end
-        # render json: user
+
+        # user = User.find_by(identifier: params[:identifier])
+
+
+        # if user
+        #     render json: user
+        # else
+        #     render 'User not in database'
+        # end
+    end
+
+    def update
+        user = redirect_to 'users'
+        if params[:name] && params[:email]
+            user.name = params[:name]
+            user.email = params[:email]
+        elsif params[:name]
+            user.name = params[:name]
+        elsif params[:email]
+            user.email = params[:email]
+        end
+
+        render json: user
+    end
+
+    def destroy
+
     end
 
 end
