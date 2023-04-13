f6e4dfbc050d43549c8a2741d85df5d86004e85c
Tyvan Cheng <tyvancheng@gmail.com>
2023-04-13 14:14:49 -0400

    as

--- routescontrollers/app/models/artwork.rb
+++ routescontrollers/app/models/artwork.rb
@@ -3,13 +3,17 @@ class Artwork < ApplicationRecord
     validates :image_url, uniqueness: true, presence: true
     validates :artist_id, presence: true
     validates :title, presence: true
-
+    
     belongs_to :artist,
         foreign_key: :artist_id,
         class_name: :User
+
     has_many :shares,
         foreign_key: :artwork_id,
-        class_name: :Artwork,
+        class_name: :ArtworkShare,
         dependent: :destroy
-    
+
+    has_many :shared_viewers,
+        through: :shares,
+        source: :viewer
 end
